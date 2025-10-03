93119b8892a76120bc75091329906b33d928ed73
Marcos Gimenes <52360413+MarcosFGimenes@users.noreply.github.com>
2025-10-03 13:15:00 -0300

    Fix signature canvas typings for dynamic import

--- src/components/signature-canvas-client.tsx
+++ src/components/signature-canvas-client.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { forwardRef } from "react";
-import SignatureCanvas from "react-signature-canvas";
+import type { ComponentClass } from "react";
+import type SignatureCanvasClass, { SignatureCanvasProps } from "react-signature-canvas";
 
-export type SignatureCanvasInstance = SignatureCanvas;
-export type SignatureCanvasProps = React.ComponentProps<typeof SignatureCanvas>;
+type SignatureCanvasInstance = SignatureCanvasClass;
+
+const SignatureCanvasDynamic = dynamic(() => import("react-signature-canvas"), {
+  ssr: false,
+}) as unknown as ComponentClass<SignatureCanvasProps>;
 
 const SignatureCanvasWithRef = forwardRef<SignatureCanvasInstance, SignatureCanvasProps>((props, ref) => (
-  <SignatureCanvas {...props} ref={ref} />
+  // @ts-expect-error – ref typing is not preserved by next/dynamic
+  <SignatureCanvasDynamic {...props} ref={ref} />
 ));
 
-SignatureCanvasWithRef.displayName = "SignatureCanvasWithRef";
+SignatureCanvasWithRef.displayName = "SignatureCanvas";
 
+export type { SignatureCanvasInstance, SignatureCanvasProps };
 export default SignatureCanvasWithRef;
