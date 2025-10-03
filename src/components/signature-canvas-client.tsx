491e21d9a93f6ded663645b63da9a4ebe5525363
Marcos Gimenes <52360413+MarcosFGimenes@users.noreply.github.com>
2025-10-03 13:09:23 -0300

    Fix PCM signature canvas export

--- src/components/signature-canvas-client.tsx
+++ src/components/signature-canvas-client.tsx
@@ -1,13 +1,19 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { forwardRef } from "react";
-import SignatureCanvas from "react-signature-canvas";
+import type { default as SignatureCanvasType } from "react-signature-canvas";
 
-export type SignatureCanvasInstance = SignatureCanvas;
-export type SignatureCanvasProps = React.ComponentProps<typeof SignatureCanvas>;
+const SignatureCanvasDynamic = dynamic(() => import("react-signature-canvas"), {
+  ssr: false,
+}) as unknown as SignatureCanvasType;
+
+export type SignatureCanvasInstance = SignatureCanvasType;
+export type SignatureCanvasProps = React.ComponentProps<SignatureCanvasType>;
 
 const SignatureCanvasWithRef = forwardRef<SignatureCanvasInstance, SignatureCanvasProps>((props, ref) => (
-  <SignatureCanvas {...props} ref={ref} />
+  // @ts-expect-error – ref typing is not preserved by next/dynamic
+  <SignatureCanvasDynamic {...props} ref={ref} />
 ));
 
 SignatureCanvasWithRef.displayName = "SignatureCanvasWithRef";
